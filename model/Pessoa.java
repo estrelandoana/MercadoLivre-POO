90f2560b7245c538dbc94aa44f6bd4d3b0a36057
Ana <lana.ohama@gmail.com>
2025-08-09 21:51:10 -0300

    Create Pessoa.java

---
+++ model/Pessoa.java
@@ -0,0 +1,37 @@
+package model;
+
+public abstract class Pessoa {
+    private String nome;
+    private String dataNascimento;
+
+    public Pessoa(String nome, String dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String mostrarInfo() {
+        return "Nome= " + nome + "\n" +
+                "DataNascimento= " + dataNascimento + "\n";
+    }
+
+    @Override
+    public String toString() {
+        return mostrarInfo();
+    }
+}
